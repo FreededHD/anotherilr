66ee5e6a7dc27b8a5ba3b82c80052088f16d5e6a
FreededHD <freededmc@gmail.com>
2020-04-11 16:24:44 +0200

    n

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on("ready", function () {
+    console.log("Bot started!");
+});
+
+client.on('message', async (message) => {
+    const content = message.content.toLowerCase();
+    if (content === 'starti' && message.member && message.member.voice && message.member.voice.channel && message.author.id === "411220992577503242") {
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play('https://streams.ilovemusic.de/iloveradio6.mp3');
+        dispatcher.setVolume(0.1);
+    }
+});
+
+client.login("Njk4NTE2NDMyMTc0MTg2NjI3.XpG-vQ.ozOWLrDGHqJ2RG72KBeojg15Yxs");
